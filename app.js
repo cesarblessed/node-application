dd8f72ab91babfc13f67c9de799a92deb95fdea5
cesinha1 <cesardepaula80@gmail.com>
2023-04-04 14:14:20 -0300

    configurando o handlebars

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer((req,res) =>  {
-    res.end("Hello world! Welvome to my website");
-}).listen(8081);
-
-console.log("O servidor rodando!");
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+/*
+var http = require('http');
+
+http.createServer((req,res) =>  {
+    res.end("Hello world! Welvome to my website");
+}).listen(8081);
+
+console.log("O servidor rodando!");
+*/
+
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
+
+
+//Template engine handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+//Conexão com o banco de dados MySQL, Sequelize
+const sequelize = new Sequelize('teste', 'root', '123456', {
+    host: "localhost",
+    dialect: 'mysql'
+});
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/html/index.html");
